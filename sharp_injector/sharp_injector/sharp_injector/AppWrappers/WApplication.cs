11c39b86b96b48fcd2a4158aa91994f85f792a30
Anton Vigen Smolarz <anton@smolarz.dk>
2022-08-27 11:38:52 +0200

    Reimplemented ClassPrinter since it was wrongfully gitignored.

--- sharp_injector/sharp_injector/sharp_injector/AppWrappers/WApplication.cs
+++ sharp_injector/sharp_injector/sharp_injector/AppWrappers/WApplication.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using BetterAW;
 using sharp_injector.Debug;
 
 namespace sharp_injector.AppWrappers
@@ -19,13 +20,9 @@ namespace sharp_injector.AppWrappers
         }
         public void Shutdown()
         {
-            FilePrinter.Print("Fuck1" + Environment.NewLine);
             var current = t_.GetProperty("Current").GetGetMethod(false).Invoke(null, null);
-            FilePrinter.Print("Fuck2" + Environment.NewLine);
             Type ct = current.GetType();
-            FilePrinter.Print("Fuck3" + Environment.NewLine);
             ct.GetMethod("Shutdown").Invoke(current, null);
-            FilePrinter.Print("Fuck4" + Environment.NewLine);
         }
 
         public System.Collections.IDictionaryEnumerator GetEnumerator()
@@ -33,13 +30,13 @@ namespace sharp_injector.AppWrappers
             var current = t_.GetProperty("Current").GetGetMethod(false).Invoke(null, null);
             if(current == null)
             {
-                FilePrinter.Print("current was null" + Environment.NewLine);
+                Terminal.Print("current was null" + Environment.NewLine);
             }
             Type ct = current.GetType();
             var resources = ct.GetProperty("Resources").GetGetMethod(false).Invoke(current, null);
             if (resources == null)
             {
-                FilePrinter.Print("resources was null" + Environment.NewLine);
+                Terminal.Print("resources was null" + Environment.NewLine);
             }
             Type rt = resources.GetType();
             var resourcesDir = (System.Collections.IDictionaryEnumerator)rt.GetProperty("GetEnumerator").GetGetMethod(false).Invoke(resources, null);
---
+++ sharp_injector/sharp_injector/sharp_injector/Debug/FilePrinter.cs
@@ -0,0 +1,12 @@
+using BetterAW;
+using Newtonsoft.Json.Linq;
+using sharp_injector.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sharp_injector.Debug {
+    public static class FilePrinter {
+    }
+}
